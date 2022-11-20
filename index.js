a33a5044a13a41a9c205774739a7015389c0ff94
afu-hive <afu.sikarinp@hivecorps.com>
2022-11-20 22:25:43 +0700

    fix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const db = require('cyclic-dynamodb')
+const CyclicDb = require("cyclic-dynamodb")
+const db = CyclicDb("sore-gold-betta-tutuCyclicDB")
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
